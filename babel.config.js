9c68bec119b59667652d6f0ab6db6c524b5015d7
Madushanka24 <madushanka20021224@gmail.com>
2025-02-28 00:22:27 +0530

    create expo project and update babel plugings

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   const plugins = [];
 
-  return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+  plugins.push('nativewind/babel');
 
+  plugins.push('expo-router/babel');
+
+  plugins.push('react-native-reanimated/plugin');
+
+  return {
+    presets: ['babel-preset-expo'],
     plugins,
   };
 };
