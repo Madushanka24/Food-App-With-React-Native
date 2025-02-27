155cd0633f74a58a353de5ac6d801da4dbd88bab
Madushanka24 <madushanka20021224@gmail.com>
2025-02-28 01:28:53 +0530

    restaurent details

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      {/* <Stack.Screen name="modal" options={{ presentation: 'modal' }} /> */}
+      <Stack.Screen name="[id]" options={{ headerShown: true, headerTitle: 'Restaurant Details' }} />
     </Stack>
   );
 }
---
+++ components/restaurentDetails.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const restaurantDetails = () => {
+    return(
+        <View>
+            <Text>restaurantDetails</Text>
+        </View>
+    )
+}
+
+export default restaurantDetails
